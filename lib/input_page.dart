96e3123f1a7ab61fbec895a6be6a142db363c278
Moein <moeinjansharifi@gmail.com>
2023-05-20 10:52:58 +0430

    make reusable card and icon content fils

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1E1C33);
@@ -24,12 +27,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReuasbleCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReuasbleCard(
-                    colour: activeCardColour,
-                  ),
+                      colour: activeCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      )),
                 ),
               ],
             ),
@@ -62,22 +72,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHeight,
           )
         ],
-      ),
-    );
-  }
-}
-
-class ReuasbleCard extends StatelessWidget {
-  ReuasbleCard({required this.colour});
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
